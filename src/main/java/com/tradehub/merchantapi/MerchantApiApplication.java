a1876e08167535ae390516c2d724e27f35f7880d
eliasfernandescout <eliasfernandescout@gmail.com>
2023-05-04 22:05:37 -0300

    first commit springboot project

---
+++ src/main/java/com/tradehub/merchantapi/MerchantApiApplication.java
@@ -0,0 +1,13 @@
+package com.tradehub.merchantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApiApplication.class, args);
+    }
+
+}
